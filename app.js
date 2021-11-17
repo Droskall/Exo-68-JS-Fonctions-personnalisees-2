3b71c3df8ab1bd79c3a9928b72c0a25c20ef80f2
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2021-11-17 14:20:05 +0100

    finish

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ function random(){
 
 let paragraphe = document.getElementsByTagName("p");
 
-for (let i = 0; i<10; i++){
+for (let i = 0; i < 10; i++){
     paragraphe[i].innerHTML = random().toString();
 }
